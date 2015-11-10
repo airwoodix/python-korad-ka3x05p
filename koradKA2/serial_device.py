86732322c3eb962eed1637462f47af6a0b8d124c
Etienne Wodey <wodey@iqo.uni-hannover.de>
2015-11-10 09:55:06 +0100

    Early pyserial wrappers

---
+++ koradKA2/serial_device.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+
+class SerialDeviceBase:
+    def __init__(self, *, encoding="ascii", read_delay=0.2):
+        self.conn = None
+        self.encoding = encoding
+        self.read_delay = float(read_delay)
+
+    @staticmethod
+    def format_command(cmd, *, channel, param, ask):
+        if channel is None and param is not None:
+            channel, param = param, channel
+
+        param = "" if param is None else ":{}".format(param)
+        channel = "" if channel is None else channel
+        ask = "" if not ask else "?"
+
+        return "{cmd}{channel}{param}{ask}".format(**locals())
+
+    def write(self, cmd, *, channel=None, param=None, ask=False):
+        msg = self.format_command(cmd, channel=channel, param=param,
+                                  ask=ask)
+        return self.conn.write(msg)
+
+    def read_all(self):
+        time.sleep(self.read_delay)
+        num_to_read = self.conn.inWaiting()
+        return self.conn.read(num_to_read)
+
+    def ask(self, cmd, *, channel=None, param=None):
+        self.write(cmd, channel=channel, param=param, ask=True)
+        return self.read_all()
+
+
+class SerialDevice(SerialDeviceBase):
+    def __init__(self, port, *, timeout=1, encoding="ascii"):
+        SerialDeviceBase.__init__(self)
+
+        import serial
+        self.conn = serial.Serial(port, baudrate=9600,
+                                  timeout=timeout)
+
+
+class TestSerialDevice(SerialDeviceBase):
+    def __init__(self):
+        SerialDeviceBase.__init__(self)
+
+        class FakeConn:
+            def write(self, msg):
+                self.msg = msg
+                print("Write: '{}'".format(msg))
+
+            def inWaiting(self):
+                return len(self.msg)
+
+            def read(self, size):
+                ret = self.msg[:size]
+                self.msg = self.msg[size:]
+                return ret
+
+        self.conn = FakeConn()
+
+    def write(self, cmd, *, channel=None, param=None, ask=False):
+        SerialDeviceBase.write(self, cmd, channel=channel, param=param,
+                               ask=ask)
+
+    def ask(self, cmd, *, channel=None, param=None):
+        a = SerialDeviceBase.ask(self, cmd, channel=channel, param=param)
+        return a

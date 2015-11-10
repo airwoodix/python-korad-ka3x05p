5fadbc62d7f8188c603b48e48a429460ecbe48ec
Etienne Wodey <wodey@iqo.uni-hannover.de>
2015-11-10 19:18:57 +0100

    Renamed package to (Py)KoradKA. Dep: pyserial

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 
 def read_version():
-    from koradKA2 import __version__
+    from koradKA import __version__
     return __version__
 
 
 setup(
-    name="PyKoradKA2",
+    name="PyKoradKA",
     description="KORAD KA Series Remote Control V2.0 Python Interface",
     author="Etienne Wodey",
     author_email="wodey@iqo.uni-hannover.de",
     version=read_version(),
-    install_requires=["decorator"],
-    packages=["koradKA2"])
+    install_requires=["pyserial"],
+    packages=["koradKA"])
